4c0010af6113cce2080a813c8ad14e16d9b0ccbc
Sebastian Kuehnau <seb@vaadin.com>
2019-09-16 09:01:16 +0200

    update docs and vaadin version no

--- configure_nexus.sh
+++ configure_nexus.sh
@@ -2,6 +2,7 @@
 
 file="/nexus-data/admin.password"
 
+# wait for admin.password file in nexus folder
 while [ ! -f "$file" ]
 do
     echo "admin.password doesn't exist yet"
@@ -9,10 +10,13 @@ do
 done
 echo "admin.password exist! Let's wait for nexus webservice"
 
+# change REST URI in script file
 sed -i "s|/service/siesta/rest/v1/script/|/service/rest/v1/script/|g" /tmp/change_password_script/change_admin_password.sh
 
+# set script as source so you can use its methods
 source /tmp/change_password_script/change_admin_password.sh 
 
+# change password
 CHANGE_ADMIN_PASSWORD_STATUS="" ;
 while [[ ${CHANGE_ADMIN_PASSWORD_STATUS} != *"succeeded!" ]]
 do
@@ -23,14 +27,10 @@ do
 done
 echo "webservice is running!"
 
+# set anomynous login to true
 cd /tmp/nexus-scripting-examples-master/simple-shell-example/
 
 sh setAnonymous.sh true
-sh create.sh npm.json
-sh run.sh npm
 
-#npm config set registry http://localhost:8081/repository/npmjs-org/ 
-
-#cd /tmp
-#cd my-nexus-project
-#mvn install -Dvaadin.version=14.0.4 -Pproduction -DskipTests
+# change password to admin
+CHANGE_ADMIN_PASSWORD_STATUS=`change_admin_password localhost:8081 admin123 admin -k`
